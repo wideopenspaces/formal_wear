1063daa24b7b47fe5600a4f9bd5df2acf35aa356
Jacob Stetser <jake@wideopenspac.es>
2013-07-05 09:51:44 -0600

    Shooting for 100%!

--- spec/completed_object_spec.rb
+++ spec/completed_object_spec.rb
@@ -19,5 +19,25 @@ describe 'a configurator with completed setup' do
         subject.docs_id.should == subject.primary.dependent_object.another_thing_to_be_configured
       end
     end
+
+    describe '#lambda_lambda_lambda' do
+      it 'equals ExternalObjectTwo.yet_another_thing_to_be_configured' do
+        subject.lambda_lambda_lambda = subject.primary.dependent_object.yet_another_thing_to_be_configured
+      end
+
+      it 'equals the method cited in its source attr' do
+        subject.lambda_lambda_lambda = subject.send(:get_pledged)
+      end
+    end
+
+    describe '#i_am_optional' do
+      it 'is defined' do
+        subject.respond_to?(:i_am_optional).should be_true
+      end
+
+      it 'is nil' do
+        subject.i_am_optional.should be_nil
+      end
+    end
   end
 end

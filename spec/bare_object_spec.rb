83903855cc2f7aa59b2f91605dff468f236bc5a5
Jacob Stetser <jake@wideopenspac.es>
2013-07-11 14:47:11 -0600

    Add a way to build new objects
    Concept: allow formal_wearing objects to set up new objects as targets for saving data.

--- spec/bare_object_spec.rb
+++ spec/bare_object_spec.rb
@@ -1,11 +1,48 @@
 require 'spec_helper'
 require_relative './support/bare_object'
+require_relative './support/new_object'
+require 'pry'
 
 describe BareObject do
   let(:obj) { BareObject }
 
   context 'including FormalWear' do
     before { obj.send(:include, FormalWear) }
+
+    context 'when build is invoked' do
+      before do
+        obj.class_eval do
+          build :new_obj, -> { NewObject.new }
+        end
+      end
+
+      it 'creates a reader called new_obj' do
+        obj.method_defined?(:new_obj).should be_true
+      end
+
+      it 'creates a method called build_new_obj' do
+        obj.method_defined?(:build_new_obj).should be_true
+      end
+
+      context 'and BareObject is instantiated' do
+        let!(:new_object) { NewObject.new }
+        subject { BareObject.new(ExternalObjectOne.new('Formal', 'Wear')) }
+
+        it 'invokes build_new_obj' do
+          BareObject.any_instance.expects(:build_new_obj).returns(true)
+          subject
+        end
+
+        it 'creates a NewObj' do
+          NewObject.expects(:new).returns(new_object)
+          subject
+        end
+
+        it 'stores NewObj in @new_obj' do
+          subject.new_obj.should be_a(NewObject)
+        end
+      end
+    end
 
     context 'when required_attr is invoked' do
       before do
---
+++ spec/support/new_object.rb
@@ -0,0 +1,2 @@
+class NewObject
+end
